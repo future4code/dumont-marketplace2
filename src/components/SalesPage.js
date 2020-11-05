c0c2d625864964acf446afdfa28bb0dc408c16c9
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2020-11-05 18:35:34 -0300

    Ajustes realizados

--- src/components/SalesPage.js
+++ src/components/SalesPage.js
@@ -1,102 +1,144 @@
 import React from "react";
-import styled from "styled-components"
-import Logo from "./imagens/futurecar.png";
+import axios from "axios";
+import Logo from "./imagens/futurecar.jpg";
 import Image from "./imagens/cars.jpg";
-import Icon from '@material-ui/core/Icon';
 import Button from '@material-ui/core/Button';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core'
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
+import { DivLogo, ImageLogo, Title, InputSale, ImageCars, ButtonConteiner } from "./styles";
 
 
-const ImageLogo = styled.img`
-   width: 200px;
-   margin-top: 0;
-` 
-
-const Tilte = styled.div`
-   font-size: 18px;
-   margin-left: 5%;
-   padding-bottom: 1%;
-`
-
-const InputSale = styled.input`
-   font-size: 18px;
-   border-color:#e0e0e0;
-   display: flex;
-   margin: 10px 30px;
-   padding: 5px;
-   width: 30%;
-`
-
-const ImageCars = styled.img`
-   width: 58%;
-   height: 9%;
-   float: right;
-   margin-top: 18%;
-  
-`
-
-// const SendButton = styled.button`
-//     font-size: 18px;
-//     background: #457B9D;
-//     color: white;
-//     width: 32.5%;
-//     margin-left: 2%;
-//     margin-bottom: 2%;
-//     padding: 1%;
-// `
 
 
-const myTheme = createMuiTheme({
-   pallete: {
-      primary: {
-         main: "#FF5C5C"
+const myTheme = createMuiTheme ({
+    pallete: {
+      primary: { 
+        main: "#FF5C5C"
       },
-      secondary: {
-         main: "#E0E0E0"
+      secondary: { 
+        main: "#E0E0E0"
       }
-   }
+    },
+    spacing: 20,
 })
 
 class SalesPage extends React.Component {
-  render() {
-    return (
-       <div className="App">
-           <div>
-             <ImageLogo src={Logo} alt="logo"/>
-             
-           <ImageCars src={Image} alt="imagem carros"/>
-           
-            <Tilte>
-                <h2>Venda seu carro Agora!! </h2>
-            </Tilte>
+    state = {
+        name: "",
+        description: "",
+        price: null,
+        paymentMethod: "",
+        shipping: null 
+    }
 
+    onChangeName= (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    onChangeDescription= (event) => {
+        this.setState({description: event.target.value})
+    }
+
+    onChangePrice= (event) => {
+        this.setState({price: event.target.value})
+    }
+
+    onChangePaymentMethod= (event) => {
+        this.setState({paymentMethod: event.target.value})
+    }
+
+    onChangeShipping= (event) => {
+        this.setState({shipping: event.target.value})
+    }
+
+
+    handleCreateAd = () => {
+        const body = {
+          name: this.state.name,
+          description: this.state.description,
+          price: this.state.price,
+          paymentMethod: this.state.paymentMethod,
+          shipping: this.state.shipping
+        };
+    
+        axios.post(
+            `https://us-central1-labenu-apis.cloudfunctions.net/futureCarOne/cars`,
+            body,
+          )
+          .then((response) => {
+            this.setState({ name: "", description: "", price: "", paymentMethod:"", shipping: "" });
+            alert("Anúncio criado com sucesso!");
+            console.log(response)
+          })
+          .catch((error) => {
+            alert("Erro ao criar anúncio!");
+            console.log(error.message);
+          });
+      };
+
+  render() {
+      console.log(this.state)
+     return (
+        <div className="App">
+            <DivLogo>
+             <ImageLogo src={Logo} alt="logo"/>
+             </DivLogo>
              <div>
+             <ImageCars src={Image} alt="imagem carros"/>
+           
+            <Title>
+                <h2>Venda seu carro Agora!!</h2>
+            </Title>
+
+            <div>
                 <InputSale placeholder = {"Nome"}></InputSale>
                 <InputSale placeholder = {"Telefone"}></InputSale>
                 <InputSale placeholder = {"Email"}></InputSale>
-                <InputSale placeholder = {"Nome do Anúncio"}></InputSale>
-               </div>
-               <div>
-                <InputSale placeholder = {"Descrição"}></InputSale>
-                <InputSale placeholder = {"Preço"}></InputSale>
-                <InputSale placeholder = {"Forma de Pagamento"}></InputSale>
-                <InputSale placeholder = {"Prazo de Entrega"}></InputSale>
-                </div>
-                <br/>
-                <MuiThemeProvider theme = {myTheme}>
-                 {/* <SendButton>ENVIAR</SendButton> */}
-                 <Button
-                     variant="contained"
-                     color={"primary"}
-                     endIcon={<Icon></Icon>}
-                     >
-                     Send
-                  </Button>
-                  </ MuiThemeProvider>
-               </div>
-               </div>
+            </div>
+            <div>
+                <InputSale 
+                    placeholder = {"Nome do Anúncio"}
+                    value = { this.state.name}
+                    onChange = {this.onChangeName}>
+                </InputSale>
+             
+                <InputSale 
+                    placeholder = {"Descrição"}
+                    value = { this.state.description}
+                    onChange = {this.onChangeDescription}>
 
-       );
-    }
- }
+                </InputSale>
+                <InputSale 
+                    placeholder = {"Preço"}
+                    value = { this.state.price}
+                    onChange = {this.onChangePrice}>
+                </InputSale>
+                <InputSale 
+                    placeholder = {"Forma de Pagamento"}
+                    value = { this.state.paymentMethod}
+                    onChange = {this.onChangePaymentMethod}>
+
+                </InputSale>
+                <InputSale 
+                    placeholder = {"Prazo de Entrega"}
+                    value = { this.state.shipping}
+                    onChange = {this.onChangeShipping}>
+                </InputSale>
+                </div>
+
+                <br/>
+
+                <MuiThemeProvider theme = {myTheme}> 
+
+                 <ButtonConteiner>
+                  <Button onClick={this.handleCreateAd} variant = "contained" color = "primary">
+                    ENVIAR
+                  </Button>
+                </ButtonConteiner>
+
+                </MuiThemeProvider>
+            </div>
+        </div>
+    );
+  }
+}
 export default SalesPage;
