351303cd813b67413a2df2e3783c98d8ee72308b
Thomas Arantes <thomas.arantes97@gmail.com>
2020-11-05 14:51:38 -0300

    o estático da página home está pronto

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Logo from './imgs/futurecar.png';
+import Button from '@material-ui/core/Button';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
+import styled from 'styled-components';
+
+const myTheme = createMuiTheme ({
+  pallete: {
+    primary: { 
+      main: "#FF5C5C"
+    },
+    secondary: { 
+      main: "#E0E0E0"
+    }
+  }
+})
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 
 export class AppContainer extends Component {
   render() {
     return (
-      <div>
-        <p>Pronto para começar!</p>
-      </div>
+      <MuiThemeProvider theme = {myTheme}> 
+
+        <LogoContainer>
+          <img src = {Logo} />
+          <h2>O que você deseja?</h2>
+        </LogoContainer>
+        
+        <ButtonContainer>
+          <Button variant = "contained" color = "primary"> 
+            Comprar 
+          </Button>
+          <Button variant = "contained" color = "primary"> Vender </Button>
+        </ButtonContainer>
+
+      </MuiThemeProvider>
     )
   }
 }
